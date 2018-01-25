99c4024090ab6634a96cfea26125ce75369877f7
TimBent <Guvhig3e>
2018-01-25 18:36:51 +0000

    Change of layout addition of new buttons. Rec doesnt work

--- app/src/main/java/com/example/jnrmint/scriberrec/MainActivity.java
+++ app/src/main/java/com/example/jnrmint/scriberrec/MainActivity.java
@@ -1,92 +1,201 @@
 package com.example.jnrmint.scriberrec;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.TextView;
 
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    //To RECORD AUDIO
     private Button stopButton;
     private Button startButton;
-    private Chronometer timer;
+
+    private Timer time;
+
+    private MediaRecorder sRecorder;
+
+    private boolean mstarted = true;
+
+    private String mFileName;
+
+    //TO PLAY AUDIO
+
+    private Button playButton;
+    private Button stopPlaying;
+
+    private MediaPlayer sPlayer;
+
+    private boolean sPlaying = true;
+
+    // Requesting permission to RECORD_AUDIO
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
     private TextView text;
-    private boolean mstarted = false;
+
     public TextView timerTextView;
-    private long startHTime = 0L;
-    private Handler customHandler = new Handler();
-    long timeInMilliseconds = 0L;
-    long timeSwapBuff = 0L;
-    long updatedTime = 0L;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timer = findViewById(R.id.timer);
+
         text = findViewById(R.id.recmsg);
+
         startButton = findViewById(R.id.btnStart);
         stopButton = findViewById(R.id.btnStop);
+
+        playButton = findViewById(R.id.btnPlay);
+        stopPlaying = findViewById(R.id.btnPause);
+
         timerTextView = findViewById(R.id.rectimer);
+        time = new Timer();
+        time.setClock(timerTextView);
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "audioTest.3gp";
+
+        //text.setText(mFileName);
 
         startButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                if(!mstarted){
+                onRecord(mstarted);
+                if(mstarted){
                     text.setText("Recording has started");
-                    timer.start();
-                    startHTime = SystemClock.uptimeMillis();
-                    customHandler.postDelayed(updateTimerThread, 0);
-                    mstarted = true;
+                    time.startTimer();
                 } else {
                     text.setText("Recording has started already!");
                 }
+                mstarted = !mstarted;
             }
         });
 
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mstarted){
+                onRecord(mstarted);
+                if(!mstarted){
                     text.setText("Recording has ended");
-                    timer.stop();
-                    timeSwapBuff += timeInMilliseconds;
-                    customHandler.removeCallbacks(updateTimerThread);
+                    time.stopTimer();
                     mstarted = false;
                 } else {
                     text.setText("Recording has stopped already!");
                 }
-
             }
         });
+        //playing and pausing playing
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPlay(sPlaying);
+                if(sPlaying){
+                    playButton.setText("Stop");
+                    text.setText("Player has Started");
+                } else {
+                    playButton.setText("Play");
+                    text.setText("Player has been stopped");
+                }
+                sPlaying = !sPlaying;
+            }
+        });
+    }
 
+    /**
+     * Recording functionality using MediaRecorder
+     */
+
+    private void onRecord(boolean start) {
+            if (start) {
+                startRecording();
+            } else {
+                stopRecording();
+            }
 
     }
 
-    private Runnable updateTimerThread = new Runnable() {
+    private void startRecording() {
+        sRecorder = new MediaRecorder();
+        sRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        sRecorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);
+        sRecorder.setOutputFile(mFileName);
+        sRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
-        public void run() {
-
-            timeInMilliseconds = SystemClock.uptimeMillis() - startHTime;
-
-            updatedTime = timeSwapBuff + timeInMilliseconds;
-
-            int secs = (int) (updatedTime / 1000);
-            int mins = secs / 60;
-            secs = secs % 60;
-            if (timerTextView != null)
-                timerTextView.setText("" + String.format("%02d", mins) + ":"
-                        + String.format("%02d", secs));
-            customHandler.postDelayed(this, 0);
+        try {
+            sRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
         }
+        sRecorder.start();
+    }
 
-    };
+    private void stopRecording() {
+        sRecorder.stop();
+        sRecorder.release();
+        sRecorder = null;
+    }
+
+    /**
+     * Playback functionality using MediaPlayer.
+     */
+
+    private void onPlay(boolean start){
+        //if(sPlayerStart) {
+            if(start) startPlayer();
+            else stopPlayer();
+        /*} else {
+            if (start) sPlayer.pause();
+            else stopPlayer(); sPlayerStart = false;
+        }*/
+    }
+
+    private void startPlayer(){
+        sPlayer.release();
+        sPlayer = new MediaPlayer();
+
+        try {
+            sPlayer.setDataSource(mFileName);
+            sPlayer.prepare();
+            sPlayer.start();
+        }   catch (IOException e){
+            Log.e(LOG_TAG, "prepare() method has failed");
+        }
+    }
+
+    private void stopPlayer(){
+        sPlayer.release();
+        sPlayer = null;
+    }
+
 
 
 
